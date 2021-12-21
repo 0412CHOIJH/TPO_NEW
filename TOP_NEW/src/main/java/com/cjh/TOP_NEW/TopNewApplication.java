6307babac595ed7924497d1436339d6e77564af1
GST <GST@DESKTOP-KJ9J2SL>
2021-12-21 14:12:50 +0900

    GIT TEST

---
+++ TOP_NEW/src/main/java/com/cjh/TOP_NEW/TopNewApplication.java
@@ -0,0 +1,22 @@
+package com.cjh.TOP_NEW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TopNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopNewApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return "hello " + name;
+	}
+
+}
---
+++ TOP_NEW/src/test/java/com/cjh/TOP_NEW/TopNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjh.TOP_NEW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
